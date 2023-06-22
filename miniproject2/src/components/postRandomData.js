9b92beeecada6f7913d7326dc35e498d80efae41
TeddPaul13 <teddtycoon@gmail.com>
2023-06-22 22:12:07 +1000

    random images displaying- consider useEffect hook

--- miniproject2/src/components/postRandomData.js
+++ miniproject2/src/components/postRandomData.js
@@ -6,24 +6,52 @@ import Button from "react-bootstrap/Button";
 const Post = () => {
   const [post, setPost] = useState(null);
 
-  useEffect(() => {
-    axios
-      .get("https://dog.ceo/api/breeds/image/random")
-      .then((response) => {
-        console.log(response)
-        setPost(response.data.message)
-      })
+  // useEffect(() =>{
+  //   const fetchImage = async()=>{
+  //     const response = await fetch("https://dog.ceo/api/breeds/image/random")
+  //   const newImage = await response.json();
+  //   console.log(newImage)
+  //     setPost(newImage.message)
+  //   }
+  //   fetchImage();
+
+  // }, [])
+
+  const handleClick = async () => {
+    try {
+      const response = await fetch("https://dog.ceo/api/breeds/image/random")
+       const newImage = await response.json();
+        console.log(newImage)
+          setPost(newImage.message)
+    } catch (err) {
+        console.log(err.message)
+    }
+}
+
+  
+
+  // useEffect(() => {
+  //   axios
+  //     .get("https://dog.ceo/api/breeds/image/random")
+  //     .then((response) => {
+  //       console.log(response)
+  //       setPost(response.data.message)
+  //     })
          
-  }, []);
+  // }, []);
 
   return (
     <>
-    {post}
-      {/* {post && (
-        <div className="position-relative">
-          <Button onclick= {Post}>Button</Button>{" "}
+      <div>
+      <img src = {post}/>
+
+      </div>
+      
+        <div >
+          <Button onClick={handleClick}>See Other Dogs</Button>{" "}
         </div>
-      )} */}
+
+      
     </>
   );
 };
