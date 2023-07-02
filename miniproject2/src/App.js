90a68db15a8cf8a42718ea525724cba364ec0101
TeddPaul13 <teddtycoon@gmail.com>
2023-07-02 22:59:29 +1000

    started a new search components for the teams

--- miniproject2/src/App.js
+++ miniproject2/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import TeamDetails from './components/TeamDetails';
+
 import DisplayTeams from './components/DisplayTeams';
 
 import OffcanvasExample from './components/navbar';
-import Post from './components/postRandomData';
+
 import SearchForm from './components/SearchForm';
 import HandleSearchForTeam from './components/HandleSearchForTeam';
 
@@ -13,11 +13,7 @@ function App() {
   <>
   <OffcanvasExample />
 
-  {/* <Post/> component for showing dog image */}
-
-  {/* <TeamDetails/> */}
-
-  {/* <DisplayTeams/> */}
+  <DisplayTeams/>
   <HandleSearchForTeam/>
  
   </>
--- miniproject2/src/components/SearchForm.js
+++ miniproject2/src/components/SearchForm.js
@@ -1,22 +1,22 @@
 import React from "react";
 import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button"
+import Button from "react-bootstrap/Button";
 
 export default function SearchForm() {
   //const [q, setQ] = useState("");
- // const [searchParam] = useState(["name"]);
+  // const [searchParam] = useState(["name"]);
   return (
     <>
       <Form className="d-flex">
-                    <Form.Control
-                      type="search"
-                      placeholder="Search"
-                      className="me-2"
-                      aria-label="Search"
-                    />
-                    <Button variant="outline-success">Search</Button>
-                  </Form>
-        {/* //value={q}
+        <Form.Control
+          type="search"
+          placeholder="Search"
+          className="me-2"
+          aria-label="Search"
+        />
+        <Button variant="outline-success">Search</Button>
+      </Form>
+      {/* //value={q}
         //onChange={(e) => setQ(e.target.value)}
       /> */}
     </>
